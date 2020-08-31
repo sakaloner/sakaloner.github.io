70bc0c99e6c6ed4b976a9e154074f0df09f9932e
sakaloner <andresf40y0@gmail.com>
2020-08-31 15:56:42 -0500

    ben

---
+++ stonks/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Stock from './stock'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Stock></Stock>
+    </div>
+  );
+}
+
+export default App;
